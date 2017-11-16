39de4ce02abb64816c3b60c9aaa3fff26690af14
rhondaqian <31214899+rhondaqian@users.noreply.github.com>
2017-11-16 15:10:32 -0500

    1116

---
+++ assignments/Final/readme.md
@@ -0,0 +1,12 @@
+# CIM640 Final Project 
+
+## Possible Ideas
+
+### 1.Use p5.play to create a digital bird, and  include some animation, mouse interaction. When you click on the cat or the button around it, the bird will make different actions.
+### 2. Use p5.collide2D to make a scene where rain keeps falling. Click the cloud button to change the speed of the rain. Use the custom draw function in p5.play to create a wind. When the wind meet the rain drop, the rain drops change their direction. Add a man with an umbrella walking on the ground. When he walks in the rain, the umbrella opens.
+### 3. Combine p5.sound and p5.particle to draw an active volcano. Use keyboard to change the flow and sound of the volcano.
+
+
+
+![idea image](https://github.com/rhondaqian/CIM640/blob/master/Final/idea1.jpg)
+
---
+++ assignments/practice5/empty-example/sketch.js
@@ -0,0 +1,75 @@
+var interfaceItems=[];
+var brushSize=10;
+var value=215;
+var value2=255;
+
+
+function setup() {
+    createCanvas(400, 400);
+    noStroke();
+    interfaceItems.push(new interface(100,100,40,60, color(255,182,193)));
+    interfaceItems.push(new interface(200,100,40,60, color(135,206,250)));
+  
+}
+
+  function draw(){
+      noStroke();
+     fill(255,value,0);
+      ellipse(mouseX,mouseY,brushSize,brushSize);
+      interfaceItems[0].check();
+      interfaceItems[0].display();
+         interfaceItems[1].check();
+            interfaceItems[1].display();
+      fill(value2);
+    ellipse(150,300,100,50);
+         }
+
+
+        function mousePressed(){
+          if(interfaceItems[0].check()==true){
+              console.log("pressed button");
+             brushSize++;
+              value= 165;
+              value2=255;
+          }
+            if(interfaceItems[1].check()==true){brushSize--;
+                                               value=215;
+                                               value2=0;}
+             
+    
+        }
+function mouseMoved() {
+  value2 = value2 - 2;
+  if (value2 <0) {
+    value2 = 255;
+  }
+}
+
+function interface(tempX,tempY,tempBoxSize1, tempBoxSize2,tempColor){
+    
+this.x=tempX;
+this.y=tempY;
+this.boxSize1=tempBoxSize1;
+this.boxSize2=tempBoxSize2;
+this.setFill=tempColor;
+this.overlay=false;
+    
+    this.display=function(){
+        fill(this.setFill);
+        rect(this.x,this.y,this.boxSize1,this.boxSize2);
+       
+        if(this.overlay == true){
+           fill(100,100);
+        rect(this.x,this.y,this.boxSize1,this.boxSize2); 
+        }
+    }
+    this.check=function(){
+        if(mouseX>this.x&&mouseX<(this.x+this.boxSize1)&&mouseY>this.y&&mouseY<(this.y+this.boxSize2)){
+            this.overlay=true;
+            return true;
+           }else{
+               this.overlay=false;
+               return false;}
+        
+    }
+}
